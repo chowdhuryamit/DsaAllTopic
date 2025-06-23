dc82ee75e82af6edff35652b886af3f002b5f84c
amit chowdhury <camit8546@gmail.com>
2025-06-23 14:49:13 +0530

    N-array tree

---
+++ N-arryTree/implementation.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+  public:
+   int data;
+   vector<TreeNode*>child;
+
+   TreeNode(int value){
+    this->data=value;
+   }
+};
+int main(){
+
+    return 0;
+}
